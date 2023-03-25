2090c13104d1d669b9456156d36368e8a2cca2c4
LuiArm <Luisarmendariz24@icloud.com>
2023-03-25 14:23:24 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by luis armendariz on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
